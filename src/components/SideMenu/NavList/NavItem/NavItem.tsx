80b59a5aeab50fbd53d0f204140e059ed7ca8c45
OECmilk <jny.adr@gmail.com>
2025-09-14 14:02:34 +0900

    Linkコンポーネントのprefetchを有効化

--- src/components/SideMenu/NavList/NavItem/NavItem.tsx
+++ src/components/SideMenu/NavList/NavItem/NavItem.tsx
@@ -17,7 +17,7 @@ const NavItem: React.FC<NavItemProps> = ({
 }) => {
     const pathname = usePathname()
   return (
-    <Link href={link} prefetch={false} className={`flex p-4 items-center w-full hover:bg-gray-700 font-medium
+    <Link href={link} prefetch={true} className={`flex p-4 items-center w-full hover:bg-gray-700 font-medium
         ${pathname === link ? 'bg-gray-600 border-r-4 border-r-green-500': ''}`}>
         <div className="mr-1">{icon}</div>
         <div>{label}</div>
